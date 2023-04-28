b33ac4ea5766f3d253105808f97b6123d6fd78dc
fadhlialfarisi46 <fadhlialfarisi46@gmail.com>
2023-04-28 15:19:55 +0700

    Initial Commit

---
+++ Heroes-App-with-SwiftUI/Heroes_App_with_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heroes_App_with_SwiftUIApp.swift
+//  Heroes-App-with-SwiftUI
+//
+//  Created by muhammad.alfarisi on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Heroes_App_with_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
